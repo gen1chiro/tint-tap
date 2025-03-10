a29d2509d0205ebf81861073aac65f07de22e9fa
gen1chiro <juljavellana1218@gmail.com>
2025-03-10 09:27:05 +0800

    add flip & snap animation

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,6 +1,12 @@
 const Tile = ({id, color, isFlipped, isCorrect, handleClick}) => {
     return (
-        <div className={`w-40 aspect-square ${isFlipped ? color : 'bg-slate-200'}`} onClick={() => handleClick(id, isFlipped, isCorrect)}>
+        <div
+            className={`w-40 aspect-square ${isFlipped ? color : 'bg-slate-200'} 
+                        ${isFlipped ? 'rotate-y-180' : 'rotate-y-[-180]'} 
+                        ${isCorrect ? 'animate-snap' : ''} 
+                        transition-transform duration-500`}
+            onClick={() => handleClick(id, isFlipped, isCorrect)}
+        >
             {color}
         </div>
     )
