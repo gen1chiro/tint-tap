48f517142d17d81423e364d022d396ced6611a67
gen1chiro <juljavellana1218@gmail.com>
2025-03-23 19:29:15 +0800

    add react confetti after game won

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import { useEffect, useState } from 'react'
+import {useEffect, useState} from 'react'
 import Tile from "./components/Tile.jsx";
-import { colorsArray, duplicateArray, shuffleArray, generateRandomId } from "./utils/utils.js";
+import DifficultyButton from "./components/DifficultyButton.jsx"
+import ResetButton from "./components/ResetButton.jsx";
+import {colorsArray, duplicateArray, shuffleArray, generateRandomId} from "./utils/utils.js";
+import Confetti from 'react-confetti'
 
 function App() {
     const [tiles, setTiles] = useState([])
@@ -22,30 +25,32 @@ function App() {
         setTiles(generateTiles(difficulty))
     }, [difficulty])
 
-  const isGameWon = tiles.every(tile => tile.isCorrect === true)
-  const totalCorrect = tiles.filter(tile => tile.isCorrect === true).length / 2
-  const totalPairs = tiles.length / 2
+    const isGameWon = tiles.every(tile => tile.isCorrect === true)
+    const totalCorrect = tiles.filter(tile => tile.isCorrect === true).length / 2
+    const totalPairs = tiles.length / 2
 
-  const handleTileMatch = ([firstTile, secondTile]) => {
-      const isMatch = firstTile.color === secondTile.color
+    const handleTileMatch = ([firstTile, secondTile]) => {
+        const isMatch = firstTile.color === secondTile.color
 
-      setTimeout(() => {setTiles(prevTiles => {
-          return prevTiles.map(prevTile => {
-              if (prevTile.id === firstTile.id || prevTile.id === secondTile.id) {
-                  return isMatch ? {
-                      ...prevTile,
-                      isCorrect: true
-                  } : {
-                      ...prevTile,
-                      isFlipped: false
-                  }
-              }
-              return prevTile
-          })
-      })}, 1000)
-  }
+        setTimeout(() => {
+            setTiles(prevTiles => {
+                return prevTiles.map(prevTile => {
+                    if (prevTile.id === firstTile.id || prevTile.id === secondTile.id) {
+                        return isMatch ? {
+                            ...prevTile,
+                            isCorrect: true
+                        } : {
+                            ...prevTile,
+                            isFlipped: false
+                        }
+                    }
+                    return prevTile
+                })
+            })
+        }, 1000)
+    }
 
-  const handleClick = (id, isFlipped, isCorrect) => {
+    const handleClick = (id, isFlipped, isCorrect) => {
         if (isFlipped || isCorrect) return
 
         setTiles(prevTiles => {
@@ -62,35 +67,40 @@ function App() {
 
             return newTiles
         })
-  }
+    }
 
-  const handleDifficultyChange = (change) => {
+    const handleDifficultyChange = (change) => {
         setDifficulty(change)
-  }
+    }
 
-  const tilesArray = tiles.map(({id, color, isFlipped, isCorrect}) => {
+    const handleGameReset = () => {
+        setTiles(generateTiles(difficulty))
+    }
+
+    const tilesArray = tiles.map(({id, color, isFlipped, isCorrect}) => {
         return (<Tile
-                    key={id}
-                    id={id}
-                    color={color}
-                    isFlipped={isFlipped}
-                    isCorrect={isCorrect}
-                    handleClick={handleClick}
-                />)
-  })
+            key={id}
+            id={id}
+            color={color}
+            isFlipped={isFlipped}
+            isCorrect={isCorrect}
+            handleClick={handleClick}
+        />)
+    })
 
-  return (
-      <main className="w-full flex flex-col justify-center items-center">
-          <h1>{totalCorrect} / {totalPairs}</h1>
-          {difficulty}
-          <button onClick={() => handleDifficultyChange(3)}>Easy</button>
-          <button onClick={() => handleDifficultyChange(6)}>Medium</button>
-          <button onClick={() => handleDifficultyChange(9)}>Hard</button>
-          <section className="w-5/6 bg-slate-300 grid grid-cols-6 grid-rows-3 gap-8 p-8">
-              {tilesArray}
-          </section>
-      </main>
-  )
+    return (
+        <main className="w-full flex flex-col justify-center items-center">
+            {isGameWon && <Confetti/>}
+            <h1>{totalCorrect} / {totalPairs}</h1>
+            <DifficultyButton onClick={handleDifficultyChange} difficultyLevel={3} difficulty={difficulty}>Easy</DifficultyButton>
+            <DifficultyButton onClick={handleDifficultyChange} difficultyLevel={6} difficulty={difficulty}>Medium</DifficultyButton>
+            <DifficultyButton onClick={handleDifficultyChange} difficultyLevel={9} difficulty={difficulty}>Hard</DifficultyButton>
+            <ResetButton onClick={handleGameReset} isGameWon={isGameWon}/>
+            <section className="w-5/6 bg-slate-300 grid grid-cols-6 grid-rows-3 gap-8 p-8">
+                {tilesArray}
+            </section>
+        </main>
+    )
 }
 
 export default App
